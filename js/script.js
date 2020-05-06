0705b8334c551684c878b3e706d94eb5c0bf5c3c
Marta Bednarz <martab2525@gmail.com>
2020-05-06 16:27:54 +0200

    add hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function toggleMenu (visible) {
   document.querySelector('.sidebar').classList.toggle('sidebar-show', visible);
+  document.querySelector('.topbar-media').classList.toggle('topbar-media-show', visible);
 }
 
 document.querySelector('.hamburger-btn').addEventListener('click', function (e) {
